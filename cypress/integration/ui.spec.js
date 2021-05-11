778c55e15c991cef3ec32943247935a47400e0f0
Niels Koster <niels.koster@portavita.nl>
2021-05-11 11:54:19 +0200

    ATM-2771: Add a 1s delay bewteen steps for demo

--- cypress/integration/ui.spec.js
+++ cypress/integration/ui.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+const DEMO = true
+
 describe('FHIR Station', () => {
 
   context('Test UI', () => {
@@ -11,19 +13,28 @@ describe('FHIR Station', () => {
           password: 'apekop',
         }
       })
+      DEMO && cy.wait(1000)
     })
 
     it('Perform a full UI walk', () => {
       cy.get('input').first().click().get('li').last().prev().prev().click()
+      DEMO && cy.wait(1000)
       cy.get('input').first().should('have.value', 'fhir3.databus.portavita.pvt_amstelveen.practitioner')
+      DEMO && cy.wait(1000)
       cy.get('label').contains('Identifier Value')
       .next().children().first().type('fleurke')
       cy.contains('"Fleurke, AM"').click()
+      DEMO && cy.wait(1000)
       cy.contains('199078').click()
+      DEMO && cy.wait(1000)
       cy.contains('1978-11-07').should('be.visible')
+      DEMO && cy.wait(1000)
       cy.get('svg').last().click()
+      DEMO && cy.wait(1000)
       cy.contains('1954-10-19').should('be.visible')
+      DEMO && cy.wait(1000)
       cy.get('svg').first().click()
+      DEMO && cy.wait(1000)
       cy.contains('"Fleurke, AM"').should('be.visible')
     })
 
